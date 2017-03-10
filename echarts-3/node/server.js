85bb26dbd096d6ca8a75b6cc8697271f37102813
LittleCrabXXY <LittleCrabXXY@163.com>
2017-03-10 16:28:26 +0800

    server.js for echarts-3

---
+++ echarts-3/node/server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+var ROOT = './';
+
+http.createServer(function(request, response) {
+    request.on('error', function(err) {
+        console.log(err);
+        response.statusCode = 400;
+        response.end();
+    });
+    response.on('error', function(err) {
+        console.log(err);
+    });
+    if (request.method === 'GET') {
+        var filepath = '';
+        if (request.url.indexOf('/lib/') === 0) {
+            filepath = ROOT + request.url;
+        } else {
+            filepath = ROOT + '/echarts-3' + request.url;
+        }
+        fs.readFile(filepath, function(err, file) {
+            if (err) {
+                console.log(err);
+                response.statusCode = 404;
+                response.end('Not Found');
+            } else {
+                response.end(file);
+            }
+        });
+    }
+}).listen(8080, 'localhost');
